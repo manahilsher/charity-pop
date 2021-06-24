692a53bea581872130f1b7e104bd5de76e4794fb
Manahil Sher <manahil.sher@gmail.com>
2021-06-24 01:03:07 -0400

    styled header some and began work on creating balloons

--- src/pages/CampaignCreate.js
+++ src/pages/CampaignCreate.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Auth } from 'aws-amplify';
 
-import { createCampaignThunk } from '../store/actions';
+import {
+  createCampaignThunk,
+  createBalloonBundleThunk
+} from '../store/actions';
 import { Link } from 'react-router-dom';
 
 class CampaignCreate extends React.Component {
@@ -16,6 +19,12 @@ class CampaignCreate extends React.Component {
       blurb: '',
       description: '',
       goal: 10
+    },
+    balloonBundle: {
+      minimum: 1,
+      maximum: 10,
+      interval: 1,
+      campaignID: ''
     }
   };
 
@@ -50,24 +59,51 @@ class CampaignCreate extends React.Component {
     if (c.name === '' || c.burb === '' || c.description === '' || c.goal < 10)
       alert('Incorrect Data');
     else {
-      await this.props
-        .createCampaignThunk({
-          ...c,
-          totalRaised: 0,
-          ownerID: this.state.user.id
-        })
-        .then(x => {
-          console.log(x);
-        });
+      await this.props.createCampaignThunk({
+        ...c,
+        totalRaised: 0,
+        ownerID: this.state.user.id
+      });
     }
   };
 
-  handleChange = event => {
+  handleCampaignChange = event => {
     this.setState({
       campaign: {
         ...this.state.campaign,
         [event.target.name]: event.target.value
       }
+    });
+  };
+
+  handleBalloonBundleChange = event => {
+    this.setState({
+      balloonBundle: {
+        ...this.state.balloonBundle,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  createBalloonBundle = async () => {
+    const { min, max, i } = this.state.balloonBundle;
+    // min is 1
+    // max is 10
+    // i is 2
+    // 1, 3, 5, 7, 9
+    // 1 + 2(n-1)
+    // how much is n
+    let totalPerRound = 0;
+    for (let x = min; x <= max; x += i) {
+      totalPerRound += x;
+    }
+    console.log(totalPerRound);
+    await this.props.createBalloonBundleThunk({
+      active: true,
+      totalRaised: 0,
+      totalPerRound,
+      roundsCompleted: 0,
+      campaignID: this.state.balloonBundle.campaignID
     });
   };
 
@@ -87,7 +123,7 @@ class CampaignCreate extends React.Component {
                 id='name'
                 name='name'
                 value={this.state.campaign.name}
-                onChange={this.handleChange}
+                onChange={this.handleCampaignChange}
               ></input>
             </div>
             <div>
@@ -97,7 +133,7 @@ class CampaignCreate extends React.Component {
                 id='id'
                 name='id'
                 value={this.state.campaign.id}
-                onChange={this.handleChange}
+                onChange={this.handleCampaignChange}
               ></input>
             </div>
             <div>
@@ -107,7 +143,7 @@ class CampaignCreate extends React.Component {
                 id='blurb'
                 name='blurb'
                 value={this.state.campaign.blurb}
-                onChange={this.handleChange}
+                onChange={this.handleCampaignChange}
               ></input>
             </div>
             <div>
@@ -117,7 +153,7 @@ class CampaignCreate extends React.Component {
                 id='description'
                 name='description'
                 value={this.state.campaign.description}
-                onChange={this.handleChange}
+                onChange={this.handleCampaignChange}
               ></input>
             </div>
             <div>
@@ -127,11 +163,57 @@ class CampaignCreate extends React.Component {
                 id='goal'
                 name='goal'
                 value={this.state.campaign.goal}
-                onChange={this.handleChange}
+                onChange={this.handleCampaignChange}
                 min='10'
               ></input>
             </div>
             <button onClick={this.createCampaign}>Create</button>
+            <div>BALLOON BUNDLE</div>
+            <div>
+              <span>Minimum: </span>
+              <input
+                type='number'
+                id='minimum'
+                name='minimum'
+                value={this.state.balloonBundle.minimum}
+                onChange={this.handleBalloonBundleChange}
+                min='1'
+              ></input>
+            </div>
+            <div>
+              <span>Maximum: </span>
+              <input
+                type='number'
+                id='maximum'
+                name='maximum'
+                value={this.state.balloonBundle.maximum}
+                onChange={this.handleBalloonBundleChange}
+                min='10'
+              ></input>
+            </div>
+            <div>
+              <span>Interval: </span>
+              <input
+                type='number'
+                id='interval'
+                name='interval'
+                value={this.state.balloonBundle.interval}
+                onChange={this.handleBalloonBundleChange}
+                min='1'
+                max='100'
+              ></input>
+            </div>
+            <div>
+              <span>Campaign ID: </span>
+              <input
+                type='text'
+                id='campaignID'
+                name='campaignID'
+                value={this.state.balloonBundle.campaignID}
+                onChange={this.handleBalloonBundleChange}
+              ></input>
+            </div>
+            <button onClick={this.createBalloonBundle}>Create</button>
           </div>
         </div>
       </>
@@ -139,4 +221,6 @@ class CampaignCreate extends React.Component {
   }
 }
 
-export default connect(null, { createCampaignThunk })(CampaignCreate);
+export default connect(null, { createCampaignThunk, createBalloonBundleThunk })(
+  CampaignCreate
+);
